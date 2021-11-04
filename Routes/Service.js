efe1fbec68c4432155780ec48318073ac0476d3b
mooka95 <mohamed.osa.22@gmail.com>
2021-11-04 05:18:30 +0200

    fixing errors

--- Routes/Service.js
+++ Routes/Service.js
@@ -7,7 +7,7 @@ const path = require('path');
 const multer  = require('multer');
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, 'images')
+    cb(null, 'Images')
   },
   filename: function (req, file, cb) {
    
--- index.js
+++ index.js
@@ -4,8 +4,9 @@ require('express-async-errors');
 const { port } =  require('./config');
 const cors = require('cors')
 require('./db')
-app.use('/uploads' , express.static('uploads'));
+
 app.use(express.json());
+app.use('/Images' , express.static('uploads'));
 
 
 const adminRoute=require('./Routes/Admin');
@@ -24,14 +25,14 @@ app.use(['/client','/Client','/clients'],clientRoute);
 app.use(['/admin','/admins'],adminRoute);
 app.use(['/service','services'],serviceRoute)
 
-// app.use((err,req,res,next)=>{
-//   err.statusCode=err.statusCode|| 500;
-//   const handleError=err.statusCode<500 ? err.message :'Something Went Wrong';
-//   res.status(err.statusCode).json({
-//     message:handleError,
-//     errors:err.errors || {}
-//   })
-// });
+app.use((err,req,res,next)=>{
+  err.statusCode=err.statusCode|| 500;
+  const handleError=err.statusCode<500 ? err.message :'Something Went Wrong';
+  res.status(err.statusCode).json({
+    message:handleError,
+    errors:err.errors || {}
+  })
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
